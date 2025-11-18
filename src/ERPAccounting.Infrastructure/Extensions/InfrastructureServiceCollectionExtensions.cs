166c5bbb639360adb35aab7b940c216cf569d3fb
Sasonal Dekant <sasonal.dekant@gmail.com>
2025-11-18 21:37:43 +0100

    Configure SQL Server infrastructure

--- src/ERPAccounting.Infrastructure/Extensions/InfrastructureServiceCollectionExtensions.cs
+++ src/ERPAccounting.Infrastructure/Extensions/InfrastructureServiceCollectionExtensions.cs
@@ -1,9 +1,11 @@
+using System;
 using ERPAccounting.Domain.Abstractions.Gateways;
 using ERPAccounting.Domain.Abstractions.Repositories;
 using ERPAccounting.Infrastructure.Data;
 using ERPAccounting.Infrastructure.Repositories;
 using ERPAccounting.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ERPAccounting.Infrastructure.Extensions;
@@ -15,8 +17,21 @@ public static class InfrastructureServiceCollectionExtensions
     /// </summary>
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
     {
-        services.AddDbContext<AppDbContext>(options =>
-            options.UseInMemoryDatabase("ERPAccounting"));
+        services.AddDbContext<AppDbContext>((serviceProvider, options) =>
+        {
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException(
+                    "DefaultConnection is not configured. Please define it in appsettings.json as described in the documentation.");
+            }
+
+            options.UseSqlServer(connectionString, sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure();
+            });
+        });
 
         services.AddScoped<IDocumentLineItemRepository, DocumentLineItemRepository>();
         services.AddScoped<IDocumentRepository, DocumentRepository>();
---
+++ tests/ERPAccounting.API.Tests/Services/InfrastructureRegistrationTests.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using ERPAccounting.Domain.Abstractions.Gateways;
+using ERPAccounting.Infrastructure.Data;
+using ERPAccounting.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace ERPAccounting.API.Tests.Services;
+
+public class InfrastructureRegistrationTests
+{
+    [Fact]
+    public void AddInfrastructureServices_UsesSqlServerProvider()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["ConnectionStrings:DefaultConnection"] =
+                    "Server=(localdb)\\mssqllocaldb;Database=ERPAccountingDb;Trusted_Connection=True;MultipleActiveResultSets=true"
+            })
+            .Build();
+
+        var services = new ServiceCollection();
+        services.AddInfrastructureServices(configuration);
+
+        using var provider = services.BuildServiceProvider();
+        using var scope = provider.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        Assert.Equal("Microsoft.EntityFrameworkCore.SqlServer", context.Database.ProviderName);
+        Assert.True(context.Database.IsRelational());
+
+        var gateway = scope.ServiceProvider.GetRequiredService<IStoredProcedureGateway>();
+        Assert.NotNull(gateway);
+    }
+}
