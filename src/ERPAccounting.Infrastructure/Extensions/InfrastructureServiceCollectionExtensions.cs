4dc8cdef526b654eb16b5bad362ed38699d24f8d
ykliugi-beep <ykliugi@gmail.com>
2025-11-19 00:16:18 +0100

    x

--- src/ERPAccounting.Infrastructure/Extensions/InfrastructureServiceCollectionExtensions.cs
+++ src/ERPAccounting.Infrastructure/Extensions/InfrastructureServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 using ERPAccounting.Application.Services;
 using ERPAccounting.Infrastructure.Services;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace ERPAccounting.Infrastructure.Extensions;
 
@@ -11,10 +10,10 @@ public static class InfrastructureServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
     {
+        services.AddDbContext<AppDbContext>((serviceProvider, options) =>
         services.AddMemoryCache();
         services.AddSingleton<CacheService>();
         services.AddScoped<ILookupService, StoredProcedureService>();
         services.AddScoped<IStoredProcedureService, StoredProcedureService>();
-        return services;
     }
 }
