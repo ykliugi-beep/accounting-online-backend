9a0811d31a41b8caf7b93007d46cbcf3f9a80399
Sasonal Dekant <sasonal.dekant@gmail.com>
2025-11-24 19:03:02 +0100

    refactor: Remove ISoftDeletable interface - soft delete will use Audit tables

--- src/ERPAccounting.Domain/Interfaces/ISoftDeletable.cs
+++
@@ -1,9 +0,0 @@
-namespace ERPAccounting.Domain.Interfaces;
-
-/// <summary>
-/// Marker interface for entities that support soft deletion via the <c>IsDeleted</c> flag.
-/// </summary>
-public interface ISoftDeletable : IEntity
-{
-    bool IsDeleted { get; set; }
-}
